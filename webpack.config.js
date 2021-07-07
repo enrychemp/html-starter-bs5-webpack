3338ad9b441b1f40141b9cfeeefea0f47c684fd3
Enrico Campani <enrico@ovostudio.it>
2021-07-07 17:27:38 +0200

    update to webpack 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,16 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: './assets/index.js',
+  entry: "./assets/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: './js/index.bundle.js',
+    publicPath: "",
+    path: path.resolve(__dirname, "dist"),
+    filename: "./js/index.bundle.js",
   },
   // Generate sourcemaps for proper error messages
-  devtool: 'source-map',
+  devtool: "source-map",
   performance: {
     // Turn off size warnings for entry points
     hints: false,
@@ -22,92 +23,105 @@ module.exports = {
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
-              publicPath: '../',
-            }
+              publicPath: "../",
+            },
           },
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
               importLoaders: 2,
-              sourceMap: true
-            }
+              sourceMap: true,
+            },
           },
           {
-            loader: 'resolve-url-loader',
+            loader: "resolve-url-loader",
           },
           {
-            loader: 'postcss-loader',
+            loader: "postcss-loader",
             options: {
-              plugins: () => [
-                require('autoprefixer')
-              ],
-              sourceMap: true
-            }
+              sourceMap: true,
+              postcssOptions: {
+                plugins: [
+                  [
+                    "postcss-preset-env",
+                    {
+                      // Options
+                    },
+                  ],
+                ],
+              },
+            },
           },
+          // {
+          //   loader: "postcss-loader",
+          //   options: {
+          //     plugins: () => [require("autoprefixer")],
+          //     sourceMap: true,
+          //   },
+          // },
           {
-            loader: 'sass-loader',
+            loader: "sass-loader",
             options: {
-              sourceMap: true
-            }
+              sourceMap: true,
+            },
           },
         ],
         exclude: /node_modules/,
       },
       {
-        enforce: 'pre', // checked before being processed by babel-loader
+        enforce: "pre", // checked before being processed by babel-loader
         test: /\.(js)$/,
-        loader: 'eslint-loader',
+        loader: "eslint-loader",
         exclude: /node_modules/,
-
       },
       {
         test: /\.(js)$/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         exclude: /node_modules/,
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
               outputPath: (url, resourcePath) => {
                 if (/icon\.png|tile\.png|tile-wide\.png/.test(resourcePath)) {
-                  return url
+                  return url;
                 } else {
-                  return `images/${url}`
+                  return `images/${url}`;
                 }
               },
-              name: '[name].[ext]',
+              name: "[name].[ext]",
             },
           },
           {
-            loader: 'image-webpack-loader',
+            loader: "image-webpack-loader",
             options: {
-              disable: process.env.NODE_ENV !== 'production', // Disable during development
+              disable: process.env.NODE_ENV !== "production", // Disable during development
               mozjpeg: {
                 progressive: true,
-                quality: 75
+                quality: 75,
               },
             },
-          }
+          },
         ],
         exclude: /node_modules/,
       },
       {
         test: /(favicon\.ico|site\.webmanifest|browserconfig\.xml|robots\.txt|humans\.txt)$/,
-        loader: 'file-loader',
+        loader: "file-loader",
         options: {
-          name: '[name].[ext]',
+          name: "[name].[ext]",
         },
         exclude: /node_modules/,
       },
       {
         test: /\.(woff(2)?|ttf|eot)(\?[a-z0-9=.]+)?$/,
-        loader: 'file-loader',
+        loader: "file-loader",
         options: {
-          outputPath: 'fonts',
-          name: '[name].[ext]',
+          outputPath: "fonts",
+          name: "[name].[ext]",
         },
         exclude: /node_modules/,
       },
@@ -116,25 +130,28 @@ module.exports = {
   // DevServer
   // https://webpack.js.org/configuration/dev-server/
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.join(
+      __dirname,
+      process.env.NODE_ENV !== "production" ? "assets" : "dist"
+    ),
     compress: true,
-    port: 9000
+    port: 9000,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './assets/html/index.html',
-      filename: 'index.html',
+      template: "./assets/html/index.html",
+      filename: "index.html",
       minify: false, // Disable minification during production mode
       hash: true,
     }),
     new HtmlWebpackPlugin({
-      template: './assets/html/404.html',
-      filename: '404.html',
+      template: "./assets/html/404.html",
+      filename: "404.html",
       minify: false, // Disable minification during production mode
       hash: true,
     }),
     new MiniCssExtractPlugin({
-      filename: './css/styles.css'
+      filename: "./css/styles.css",
     }),
-  ]
-}
+  ],
+};
